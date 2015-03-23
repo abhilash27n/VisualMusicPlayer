69809799c88da08d470abe1936cd0f59c72b3ecf
abhilash27n <abhilash.27n@gmail.com>
2015-03-22 23:46:51 -0400

    Get no of songs by country working with simple query

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 
 3) nodemon bin/www  - if nodemon exists, restarts node whenever files are changed
 
+4) Database details in index.js need to be configured
+
+5) Followed this Tutorial for Setting up nodeJS
+https://www.youtube.com/watch?v=FqMIyTH9wSg
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
+
+
+//Creating mySQL connection
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'start',
+  database : 'Database_Project_DB'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', {});
+});
+
+router.post('/', function(req, res){
+	//res.send('COUNTRY SUBMITTED')
+	var country = req.body.country;
+	//execute query
+	connection.query('SELECT count(*) as NUM_SONGS from Songs where songCountry = "'+country+'"', function(err, rows, fields) {
+	if (!err){
+	    console.log('The solution is: ', rows);
+	    res.render('index', { no_of_songs: rows[0].NUM_SONGS });
+	}
+	else
+	    console.log('Error while performing Query.');
+	});
+
 });
 
 module.exports = router;
