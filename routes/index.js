c777413a1e6c74812b4db551b2345c7a88fecbf9
abhilash27n <abhilash.27n@gmail.com>
2015-03-30 21:54:32 -0400

    Cleaned up code version 1.0

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,6 @@ var connection = mysql.createConnection({
 });
 
 
-//Testing world map
-/*router.get('/world', function(req, res, next) {
-
-	res.sendfile("views/zoomingToCountries.html");
-});
-*/
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -34,37 +26,21 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res){
-	//res.send('COUNTRY SUBMITTED')
 	//POST REQUEST
 	var country = req.body.country;
 	var fromYear = req.body.fromYear;
 	var toYear = req.body.toYear;
-	//GET REQUEST
-	//var country = req.query.country;
-	//var fromYear = req.query.fromYear;
-	//var toYear = req.query.toYear;
 
 	//execute query
-	query1 = 'SELECT count(*) as NUM_SONGS from Songs where songCountry = "'+country+'"';
-	query2 = 'SELECT youtubeId as url from Songs where songCountry = "'+country+'" LIMIT 1';
-	query2_1 = 'SELECT youtubeId as url from Songs where songCountry = "'+country+'"';
-	query3 = 'SELECT youtubeId as url from Songs where songCountry = "'+country+'" and releaseDate >= '+fromYear+' and releaseDate <= '+toYear;
-	//connection.query(query1, function(err, rows, fields) {
-	// if (!err){
-	//     console.log('The solution is: ', rows);
-	//     res.render('index', { no_of_songs: rows[0].NUM_SONGS });
-	// }
-	// else
-	//     console.log('Error while performing Query.');
-	// });
-	connection.query(query3, function(err, rows, fields) {
+	query = 'SELECT youtubeId as url from Songs where songCountry = "'+country+'" and releaseDate >= '+fromYear+' and releaseDate <= '+toYear;
+
+	connection.query(query, function(err, rows, fields) {
 	if (!err){
 	    //console.log('The solution is: ', rows)
 	    no_songs = rows.length;
 	    console.log("No of songs returned: "+no_songs);
 	    if(no_songs == 0){
 	    	//No songs returned
-	    	//res.render('index', { youtube_link: "", no_of_songs: no_songs, country_name: country, from_year: fromYear, to_year: toYear });
 	    	res.send(JSON.stringify("NoSongsReturned"));
 	    }
 	    else{
@@ -72,7 +48,6 @@ router.post('/', function(req, res){
 		    var rand =randomIntFromInterval(0, no_songs-1);
 		    console.log("Random song selected: "+rand);
 		    link = rows[rand].url;
-		    //res.json({ youtube_link: link, no_of_songs: no_songs, country_name: country, from_year: fromYear, to_year: toYear });
 		    res.send(JSON.stringify(link));
 	   }
 	    
